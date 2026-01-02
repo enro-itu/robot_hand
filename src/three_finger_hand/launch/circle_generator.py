d91d3bc3fc16b3ee10031800e799fa053420e740
ErenDikkulah <erendikkulah@outlook.com.tr>
2026-01-02 17:54:37 +0300

    add function to visualize circlular path

--- src/three_finger_hand/launch/circle_generator.py
+++ src/three_finger_hand/launch/circle_generator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 def generate_circle_points(center, normal, radius, N):
     center = np.array(center)
@@ -16,9 +17,19 @@ def generate_circle_points(center, normal, radius, N):
     points = [center + radius * np.cos(t) * u + radius * np.sin(t) * v for t in thetas]
     return np.array(points)
 
+def visualize_circle(points, center, normal):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(points[:,0], points[:,1], points[:,2], "-o", label='Circlular path')
+    ax.scatter(center[0], center[1], center[2], color='red', label='Center')
+    ax.quiver(center[0], center[1], center[2], normal[0], normal[1], normal[2], length=0.1, color='green', label='Normal')
+    ax.set_xlabel('X'); ax.set_ylabel('Y'); ax.set_zlabel('Z')
+    plt.legend()
+    plt.show()
+
 if __name__ == "__main__":
     center = [-0.02845557, 0.04310951, 0.12971483]
-    normal = [0, 0, 1]
+    normal = [0, 0.6, 0.8]
     radius = 0.001
     N = 50
 
@@ -27,3 +38,4 @@ if __name__ == "__main__":
     os.makedirs('src', exist_ok=True)
     np.save('src/waypoints.npy', waypoints)
     print(f"{N} waypoints saved to 'src/waypoints.npy'.")
+    visualize_circle(waypoints, center, normal)
